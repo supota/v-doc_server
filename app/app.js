75240c0163fbb84d8d76588ad61bfc8b6a8764f7
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-14 22:22:00 +0900

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+var router = require('./routes/v1/');
+app.use('/api/v1/', router);
+
+//サーバ起動
+app.listen(port);
+console.log('listen on port ' + port);
