640f7b8936108cdaeb994b271cb70b570da8b0ed
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-18 08:18:03 +0900

    🤩: v1/player のルートAPI設定

--- app/models/routes/v1/player.js
+++ app/models/routes/v1/player.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// GET  http://localhost:3000/v1/player
+router.get('/', function (req, res){
+    res.json({
+        message: "This is player api"
+    })
+});
+
 // GET  http://localhost:3000/v1/player/test
 router.get('/test', function (req, res) {
     res.json({
-        message: "This is player api"
+        message: "This is player api - test"
     });
 });
 
