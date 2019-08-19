1612dfdeda1897edd3d0b66e5e3f55d5ca6192ff
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-19 13:41:18 +0900

    🤩: MySQL-connection

--- app/models/routes/v1/index.js
+++ app/models/routes/v1/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-let connection = require('../../../mysql');
 
 router.use('/player', require('./player.js'));
 
--- app/models/routes/v1/player.js
+++ app/models/routes/v1/player.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const os = require('os');
-const hostname = os.hostname();
+const moment = require('moment');
+let connection = require('../../../mysql');
 
 // GET  http://localhost:3000/v1/player
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
     const getPlayerData = [
         { name: '今井美香', sex: 0 },
         { name: '紀川孝太', sex: 1 },
@@ -15,13 +15,14 @@ router.get('/', function (req, res) {
 });
 
 // POST  http://localhost:3000/v1/player
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     let playerList = []
     
     const postPlayerData = req.body;
     const postPlayerName = postPlayerData.title;
     const postPlayerSex = postPlayerData.sex
     const playerId = uuidv4();
+    const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
     
     const playerItem = {
         playerId,
@@ -30,7 +31,13 @@ router.post('/', (req, res) => {
     };
     playerList.push(playerItem);
     res.json(playerItem);
-    console.log("POST: " + req.protocol + '://' + req.headers.host + req.url + "v1/player")
+
+    let query = `INSERT INTO boards (title, created_at) VALUES (${title}, ${createdAt})`;
+    connection.query(query, function (err, rows) {
+        res.redirect('/');
+    });
+
+    console.log("POST: " + req.protocol + '://' + req.headers.host + req.url + "v1/player" + "/n    : " + playerItem)
 });
 
 //routerをモジュールとして扱う準備
