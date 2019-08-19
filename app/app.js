8114aa1bfabace2bde13ea237b18f284570c4d89
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-19 15:00:45 +0900

    🤩: Helmet

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ const app = express();
 const bodyParser = require('body-parser');
 let router = require('./models/routes/v1/');
 const port = process.env.PORT || 3000;
+const helmet = require("helmet");
 
 app.use(multer().none());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(helmet());
 app.use('/v1/', router);
 app.listen(port, () => console.log('PORT:' + port + '\n---------'));
